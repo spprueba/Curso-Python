c7c2d8800eed255adf39c0498448a0ec5b97b634
spprueba <spprueba@users.noreply.github.com>
2015-06-15 10:43:54 +0200

    Hemos hecho un cambio

---
+++ Prueba_commit.py
@@ -0,0 +1,16 @@
+'''
+Created on 15/06/2015
+
+@author: EPPE\spr
+
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+'''
+def add(a,b):
+    return a+b
+
+def addFixedValue(a):
+    y = 5
+    return y +a
+  
+print add(1,2)
+print addFixedValue(1) 
